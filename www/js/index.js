11086c46e2eb3d74dfb51d9c17fe4d73d25de3d5
Nick Doiron <ndoiron@mapmeld.com>
2016-01-05 23:58:55 +0530

    initial cordova commit

---
+++ www/js/index.js
@@ -0,0 +1,28 @@
+var app = {
+    initialize: function() {
+        this.bindEvents();
+    },
+
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+        console.log(navigator.camera);
+        camera.getPicture(function() { }, function() { });
+    },
+
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+app.initialize();
